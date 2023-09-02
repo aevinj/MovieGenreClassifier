fb17f58981a14e58900c0967900cb0479818bad9
aevinj <ajjaevinjais@gmail.com>
2023-09-02 23:47:32 +0100

    final refinements

--- main.py
+++ main.py
@@ -39,19 +39,29 @@ def tokenize_chunk(chunk):
     return chunk
 
 class MovieGenreClassifier:
-    def __init__(self, data_path):    
-        dataset = pd.read_csv(data_path)
+    def __init__(self):    
+        dataset = pd.read_csv('wiki_movie_plots_deduped.csv')
         columns_to_keep = ['Genre', 'Plot']
         self.data = dataset[columns_to_keep]
         self.lemmatizer = WordNetLemmatizer()
         self.vectorizer = TfidfVectorizer(max_features=1000)
-        self.processDataset()
+        self.label_encoder = LabelEncoder()
+        if not (os.path.exists(os.path.join('model', 'trained_model.pkl')) and os.path.exists(os.path.join('model', 'tfidf_vectorizer.pkl')) and os.path.exists(os.path.join('model', 'label_encoder.pkl'))):
+            self.processDataset()
+        else:
+            print("Existing model found...")
+            self.load_trained_model()
         
+    def load_trained_model(self):
+        self.trained_model = joblib.load(os.path.join('model', 'trained_model.pkl'))
+        # Load the TF-IDF vectorizer as well
+        self.vectorizer = joblib.load(os.path.join('model', 'tfidf_vectorizer.pkl'))
+        # Finally load the label encoder
+        self.label_encoder = joblib.load(os.path.join('model', 'label_encoder.pkl'))
+    
     def processDataset(self):
         print("Processing dataset...")
-        print("Removing duplicates and empty values...")
-        self.data.drop_duplicates(inplace=True) #remove duplicates
-        self.data.dropna(inplace=True)  #remove empty values
+        self.data = self.data[self.data['Genre'] != 'unknown']
         
         print("Removing stop words and lemmatizing...")
         self.data['Plot'] = self.data['Plot'].apply(self.preprocess_text)
@@ -66,9 +76,7 @@ class MovieGenreClassifier:
         print("Feature extraction...")
         self.TFIDFMatrix = self.vectorizer.fit_transform(self.data['Tokenized_Plot'].apply(lambda x: ' '.join(x))).toarray()
 
-        label_encoder = LabelEncoder()
-        self.data['Encoded_Genre'] = label_encoder.fit_transform(self.data['Genre'])
-        self.label_encoder = label_encoder
+        self.data['Encoded_Genre'] = self.label_encoder.fit_transform(self.data['Genre'])
 
         # Train-Test Split
         X_train, X_test, y_train, y_test = train_test_split(
@@ -91,16 +99,13 @@ class MovieGenreClassifier:
         return ' '.join(words)
     
     def trainModel(self, X_train, y_train):
-        if os.path.exists('trained_model.pkl'):
-            print("Existing model found...")
-            self.trained_model = joblib.load('trained_model.pkl')
-            return self.trained_model
-        else:
-            print("Training the model...")
-            model = LogisticRegression(max_iter=100)  # You can adjust hyperparameters
-            model.fit(X_train, y_train)
-            self.trained_model = model
-            joblib.dump(model, 'trained_model.pkl')
+        print("Training the model...")
+        model = LogisticRegression(max_iter=100)  # You can adjust hyperparameters
+        model.fit(X_train, y_train)
+        self.trained_model = model
+        joblib.dump(model, os.path.join('model', 'trained_model.pkl'))
+        joblib.dump(self.vectorizer, os.path.join('model', 'tfidf_vectorizer.pkl'))
+        joblib.dump(self.label_encoder, os.path.join('model', 'label_encoder.pkl'))
         return model
 
     def evaluateModel(self, model, X_test, y_test):
@@ -108,24 +113,26 @@ class MovieGenreClassifier:
         y_pred = model.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
         print("Accuracy:", accuracy)
-        
-    def preprocess_input_plot(self, input_plot):
-        preprocessed_plot = self.preprocess_text(input_plot)
-        return preprocessed_plot
     
     def predict_genre(self, input_plot):
-        preprocessed_plot = self.preprocess_input_plot(input_plot)
+        preprocessed_plot = self.preprocess_text(input_plot)
         tfidf_matrix = self.vectorizer.transform([preprocessed_plot]).toarray()
         encoded_genre = self.trained_model.predict(tfidf_matrix)[0]
         predicted_genre = self.label_encoder.inverse_transform([encoded_genre])[0]
         return predicted_genre
 
 if __name__ == "__main__":
-    data_path = 'C:/Users/aevin/Desktop/wiki_movie_plots_deduped.csv'
-    movie_classifier = MovieGenreClassifier(data_path)
-    
-    input_plot = "In this uproarious film a bachelor party in Las Vegas spirals into a wild and unforgettable adventure. When three friends wake up with no memory of the previous night, they must retrace their steps to find their missing groom-to-be. Hilarity ensues as they encounter eccentric characters, unexpected challenges, and a trail of chaos that threatens to derail the upcoming wedding. With the clock ticking, the trio races against time to piece together the puzzle of their night of debauchery. Filled with outrageous antics and laugh-out-loud moments, this film showcases the unpredictable nature of friendship and the joys of embracing the unexpected."
-    predicted_genre = movie_classifier.predict_genre(input_plot)
+    while True:
+        choice = input("Would you like to use your custom pnlot to test the model? (default is a comedy) Y/N \n")
+        if choice.upper() == 'Y' or choice.upper() == 'N':
+            if choice.upper() == 'N':
+                plot = "In this uproarious film a bachelor party in Las Vegas spirals into a wild and unforgettable adventure. When three friends wake up with no memory of the previous night, they must retrace their steps to find their missing groom-to-be. Hilarity ensues as they encounter eccentric characters, unexpected challenges, and a trail of chaos that threatens to derail the upcoming wedding. With the clock ticking, the trio races against time to piece together the puzzle of their night of debauchery. Filled with outrageous antics and laugh-out-loud moments, this film showcases the unpredictable nature of friendship and the joys of embracing the unexpected."
+            else:
+                plot = input("Enter movie plot:\n")
+            break
+            
+    movie_classifier = MovieGenreClassifier()
+    predicted_genre = movie_classifier.predict_genre(plot)
     
     print("Predicted Genre:", predicted_genre)
     
